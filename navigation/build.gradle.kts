2e2dfe6031727471dd1e257df01cd73d21d8fa7c
Harun Bekcan <harunbekcan@gmail.com>
2023-04-28 15:25:32 +0300

    NavigationGradle fixed.

--- navigation/build.gradle.kts
+++ navigation/build.gradle.kts
@@ -32,12 +32,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation (Dependencies.androidCoreKtx)
+    implementation (Dependencies.appCompat)
+    implementation (Dependencies.material)
+    testImplementation (Dependencies.junit4)
+    androidTestImplementation (Dependencies.junitExtensions)
+    androidTestImplementation (Dependencies.espressoCore)
+
+    implementation(Dependencies.navigationUi)
+    implementation(Dependencies.navigationFrag)
+
 }
